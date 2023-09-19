f39c48ebb455ed24911e6714f6c3054a495b53de
Ahmet Hakan Aktuğ <ahmet.aktug@tubitak.gov.tr>
2023-09-19 09:31:27 +0300

    File transfer error fixed

--- src/file_op.rs
+++ src/file_op.rs
@@ -14,11 +14,7 @@ pub fn send_file(mut file: std::fs::File, socket: &mut TcpStream) {
         if let Command::FileAck(next) = proto::recieve_request(socket) {
             if next.is_some() {
                 let next = next.unwrap();
-                file.seek(SeekFrom::Start(MB * next))
-                    .expect("Cannot seek that point!");
-                let mut file_seg: Vec<u8> = vec![0; (MB as usize) - 100];
-                let read_into_len = file.read(&mut file_seg).unwrap();
-                file_seg.truncate(read_into_len);
+                let file_seg = get_file_segment(&mut file, next);
                 let mut file_pb = crate::protocom::response::Response::default();
                 let _ = file_pb.response_type.insert(
                     crate::protocom::response::response::ResponseType::File(
@@ -52,4 +48,13 @@ pub fn file_transfer_routine(socket: &mut TcpStream, filename: String) {
             panic!("Expected a file accept, got another command type.");
         }
     }
+}
+
+fn get_file_segment(file: &mut std::fs::File, segment_no: u64) -> Vec<u8> {
+    file.seek(SeekFrom::Start((MB - 100) * segment_no))
+        .expect("Cannot seek that point!");
+    let mut file_seg: Vec<u8> = vec![0; (MB as usize) - 100];
+    let read_into_len = file.read(&mut file_seg).unwrap();
+    file_seg.truncate(read_into_len);
+    file_seg
 }
